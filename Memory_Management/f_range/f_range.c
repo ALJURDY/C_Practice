1938c9032e918abef980db336db884a1858ce6c3
Fahd <fahd.aljordy@gmail.com>
2026-04-01 16:01:59 +0200

    Add f_range directory, f_range.c

---
+++ Memory_Management/f_range/f_range.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	*f_range(int min, int max)
+{
+	int	i;
+	int	*table;
+
+	if (min >= max)
+		return (NULL);
+	table = malloc(sizeof(int) * (max - min));
+	i = 0;
+	while (min < max)
+		table[i++] = min++;
+	return (table);
+}
+
+int	main()
+{
+	int *str;
+	int i = 0;
+	str = f_range(0,5);
+
+	while (str[i] < 42)
+	{
+		printf("%d",str[i]);
+		i++;
+	}
+	printf("\n");}
+
