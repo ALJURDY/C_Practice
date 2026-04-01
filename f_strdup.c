e5ec74a6a4119d553da63f5d13cdfb92723d3306
Fahd <fahd.aljordy@gmail.com>
2026-04-01 11:43:51 +0200

    Add f_strdup.c

---
+++ f_strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	if (!src)
+		return (NULL);
+	dest = malloc(ft_strlen(src) * sizeof(char) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int main()
+{
+	char src[] = "This is a test";
+//	char src1[] = "\0";
+	char *dest = ft_strdup(src);
+	printf("%s\n", dest);
+}
+*/
